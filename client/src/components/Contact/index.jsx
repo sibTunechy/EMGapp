2a2cc9011a432fa45c5e67bed942e0c0485ae23f
Ahm <ayotemitope930@gmail.com>
2022-06-28 08:34:02 +0100

    more css

--- client/src/components/Contact/index.jsx
+++ client/src/components/Contact/index.jsx
@@ -4,24 +4,27 @@ import '../../styles/Contact.css';
 
 function Contact() {
   return (
-    <div className='contact'>
-      <div className='leftSide' style={{backgroundImage: `url(${Contactimg})`}}>
-      </div>
-      <div className='rightSide'>
-        <h1>Contact Us</h1>
+    <>
+      <div className='topdiv'></div>
+      <div className='contact'>
+        <div className='leftSide' style={{backgroundImage: `url(${Contactimg})`}}>
+        </div>
+        <div className='rightSide'>
+          <h1>Contact Us</h1>
 
-        <form contact-form method='POST'>
+          <form contact-form method='POST'>
 
-          <label htmlFor='name'>Full Name</label>
-          <input name='name' placeholder='Enter Full Name...' type='text' />
-          <label htmlFor='email'>Email</label>
-          <input name='email' placeholder='Enter email...' type='email' />
-          <label htmlFor='message'>Message</label>
-          <textarea rows='6' placeholder='Enter message...' name='message' required ></textarea>
-          <button type='submit'> Send Message </button>
-        </form>
+            <label htmlFor='name'>Full Name</label>
+            <input name='name' placeholder='Enter Full Name...' type='text' />
+            <label htmlFor='email'>Email</label>
+            <input name='email' placeholder='Enter email...' type='email' />
+            <label htmlFor='message'>Message</label>
+            <textarea rows='6' placeholder='Enter message...' name='message' required ></textarea>
+            <button type='submit'> Send Message </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
