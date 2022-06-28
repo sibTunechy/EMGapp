163c4083d0a73e2956dfd5d8a73362540d5d7128
Ahm <ayotemitope930@gmail.com>
2022-06-28 17:39:36 +0100

    more

--- client/src/components/ContactPage/index.jsx
+++ client/src/components/ContactPage/index.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 const ContactPage = () => {
   return (
     <>
-        
+        <div className='wrapper'>
+            <div className='contact-form'>
+                <div className='input-fields'>
+                    <input type='text' class='input' placeholder='Name' />
+                    <input type='text' class='input' placeholder='Email Address' />
+                    <input type='text' class='input' placeholder='Phone' />
+                    <input type='text' class='input' placeholder='Subject' />
+                </div>
+                <div className='message'>
+                    <textarea placeholder='Message' ></textarea>\
+                    <div className='btn'>Send</div>
+                </div>
+            </div>
+        </div>
     </>
   )
 }
