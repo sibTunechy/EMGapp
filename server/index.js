48a13313f1fbc1119fe5be7e24ec5399f0b45e67
Ahm <ayotemitope930@gmail.com>
2022-06-28 01:09:59 +0100

    got heroku job done

--- server/index.js
+++ server/index.js
@@ -27,16 +27,34 @@ app.get('/', (req, res) => {
 
 // const CONNECTION_URL = 'mongodb+srv://EmgNew:emgpassword.@cluster0.doo3b.mongodb.net/?retryWrites=true&w=majority';
 // const CONNECTION_URL = 'mongodb+srv://EmgNew:emgpassword.@cluster0.doo3b.mongodb.net/?retryWrites=true&w=majority';
-const host = 'localhost';
-const port = 5000;
+
+const host = '0.0.0.0';
+const port = process.env.PORT || 5000; // this goes with port and host // got the job done (i)
 
 // app.listen (process.env.PORT || 5000, function() {
 //     console.log('Express server listening on port', this.address().port, app.settings.env);
 // });
 
 app.listen(port, host, function() {
-    console.log('Server started.....')
+     console.log('Server started.....')        // got the job done (ii)
 })
+
+// var port_number = server.listen(process.env.PORT || 5000);
+// app.listen(port_number);
+
+
+// var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+// var server_host = process.env.YOUR_HOST || '0.0.0.0';
+// server.listen(server_port, server_host, function() {
+//     console.log('Listening on port....', server_port);
+// })
+
+// const {PORT=3000, LOCAL_ADDRESS='0.0.0.0'} = process.env
+// server.listen(PORT, LOCAL_ADDRESS, () => {
+//     console.log('server listening at', address)
+// })
+
+// .listen(process.env.PORT || 5000)  // second .listen
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
