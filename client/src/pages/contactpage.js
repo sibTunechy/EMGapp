190e5a9a15715cf73d6f0bc0bcb6a01482ff385b
Ahm <ayotemitope930@gmail.com>
2022-06-30 21:07:00 +0100

    more

--- client/src/pages/contactpage.js
+++ client/src/pages/contactpage.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Contact from '../components/Contact';
+// import Contact from '../components/Contact';
+import ContactPage from '../components/ContactPage';
 
 const contactpage = () => {
   return (
     <>
-        <Contact/>
+        <ContactPage/>
     </>
   )
 }
