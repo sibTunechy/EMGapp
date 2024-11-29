f83a64db5d9b1ed3a78f87b79372d235f869d315
Ahmed Abdullahi <ayotemitope930@gmail.com>
2024-11-29 17:13:23 +0100

    more changes added

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://emg-app.herokuapp.com' }); // https://localhost:5000 was replaced by heroku
+const API = axios.create({ baseURL: 'https://emg-app.herokuapp.com' });  // https://localhost:5000 was replaced by heroku
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
